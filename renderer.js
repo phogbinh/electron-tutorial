a63ec4fa236e8b35aa8b02f56fd4f8802601eb3b
phongbinht <phongbinht@synology.com>
2024-01-17 14:32:28 +0800

    feat: add ipc

--- renderer.js
+++ renderer.js
@@ -1,2 +1,9 @@
 const information = document.getElementById('info')
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+
+const func = async () => {
+  const ipcMessage = document.getElementById('ipc-message')
+  ipcMessage.innerText = await window.versions.ping()
+}
+
+func()
